6d7d6e8fa31c143a6fa0074edbee9d23e2533c2a
Justin Xin <Tsubuzaki@users.noreply.github.com>
2016-05-07 20:30:33 +0800

    added app base

---
+++ app/src/main/java/org/sp/attendance/ATSLoginActivity.java
@@ -0,0 +1,13 @@
+package org.sp.attendance;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ATSLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_atslogin);
+    }
+}
