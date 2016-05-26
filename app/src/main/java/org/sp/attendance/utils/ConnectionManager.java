c9ba48e3a650b60d776b2787874c0a0af37c1d0b
Justin Xin <Tsubuzaki@users.noreply.github.com>
2016-05-26 08:36:58 +0800

    added Jsoup library

--- app/src/main/java/org/sp/attendance/utils/ConnectionManager.java
+++ app/src/main/java/org/sp/attendance/utils/ConnectionManager.java
@@ -1,10 +1,9 @@
 package org.sp.attendance.utils;
 
 /**
- * Created by HexGate on 7/5/16.
+ * Created by HexGate on 9/5/16.
  */
+
 public class ConnectionManager {
 
-    public static String attendanceCode;
-    //TODO: Migrate ATS_NativeUI code
 }
