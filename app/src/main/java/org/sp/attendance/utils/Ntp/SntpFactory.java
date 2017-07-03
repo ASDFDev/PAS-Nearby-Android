a417346d95b3837fc5d8ec0649782c501071f8c0
Daniel Quah <danielquah148@yahoo.com>
2017-07-03 22:26:14 +0800

    Fix a bug where on certain networks, we are unable to get time

--- app/src/main/java/org/sp/attendance/utils/Ntp/SntpFactory.java
+++ app/src/main/java/org/sp/attendance/utils/Ntp/SntpFactory.java
@@ -26,6 +26,10 @@ import com.github.ybq.android.spinkit.style.Wave;
 
 import org.sp.attendance.R;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
 import java.util.Date;
 
 class SntpFactory extends AsyncTask<Void, Void, Date> {
@@ -51,21 +55,50 @@ class SntpFactory extends AsyncTask<Void, Void, Date> {
     }
 
 
+
     @Override
     protected Date doInBackground(Void... params) {
         SntpClient client = new SntpClient();
-        if (client.requestTime("time.google.com", 20000 /* 20 seconds timeout */)) {
-            long now =
-                    client.getNtpTime() + SystemClock.elapsedRealtime() - client.getNtpTimeReference();
-            date = new Date(now);
-            return date;
+        String REMOTE_SERVER = "time.google.com";
+        if (client.requestTime(REMOTE_SERVER, 2000 /* 2 seconds timeout */)) {
+                long now =
+                        client.getNtpTime() + SystemClock.elapsedRealtime() - client.getNtpTimeReference();
+                date = new Date(now);
+                return date;
+            }
+        /*
+        On certain networks(such as Singapore Polytechnic), NTP requests are blocked by the firewall,
+        a workaround is used. We will send header requests to a remote server and read the header reply.
+        A typical web server will reply with tons of information, one of which contain a date.
+
+        This is known as NTP over https. It is much more secure however, we will not be conforming with
+        RFC 4330 (SNTP)
+        */
+        if(date == null) {
+            try {
+                URL url = new URL("https://" + REMOTE_SERVER);
+                URLConnection conn = url.openConnection();
+                if (conn instanceof HttpURLConnection) {
+                    HttpURLConnection httpConn = (HttpURLConnection) conn;
+                    httpConn.setRequestMethod("HEAD");
+                    long dateTime = conn.getHeaderFieldDate("Date", 0);
+                    if (dateTime > 0) {
+                        date = new Date(dateTime);
+                        return date;
+                    }
+                }
+            } catch (IOException ioe) {
+                System.out.println("Unable to get any clock! " + ioe);
+            }
         }
         return date;
     }
 
     @Override
     protected void onPostExecute(Date currentTime){
-        super.onPostExecute(date);
         progressDialog.dismiss();
+        super.onPostExecute(date);
+
     }
+
 }
